0702451c868bfa2c6aca01a7ab06f4cae5f1ac64
Greg Loeffelholz <gloeff@yahoo.com>
2026-06-08 09:52:57 -0400

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: "Job Search Command Center MCP" };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
